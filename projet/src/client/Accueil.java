00239237fff6e23c8f3ad69ba08e262ae65919d5
Thomas Salmon <th_s@hotmail.fr>
2013-04-28 12:07:41 +0200

    html pour plusieurs lignes

--- projet/src/client/Accueil.java
+++ projet/src/client/Accueil.java
@@ -52,7 +52,7 @@ public class Accueil extends JPanel
     JButton view_periph = new JButton("Voir périphériques");
     
     //text
-    JTextField recherche = new JTextField(10);
+    JTextField recherche = new JTextField(30);
     
     Accueil()
     {
@@ -63,7 +63,7 @@ public class Accueil extends JPanel
 	conteneur_est.setLayout(new GridLayout(6,1));
 	conteneur_centre.setLayout(new GridLayout(6,1));
 	conteneur_ouest.setLayout(new GridLayout(6,1));
-
+	
 	content_deco.add(deconnexion) ;
 	content_recherche.add(recherche); 
 	content_tout.add(view_all);
@@ -71,42 +71,40 @@ public class Accueil extends JPanel
 	content_titre_reco.add(new JLabel("Applications recommandées"));
 	content_titre_top.add(new JLabel("Top Applications"));
 	//need req
-	content_info.add(new JLabel("Nom: "));
-	content_info.add(new JLabel("Prenom: "));
-	content_nb.add(new JLabel("Mela: "));
-	content_nb.add(new JLabel("Nombre App: "));
+	content_info.add(new JLabel("<html>Nom: <br/>Prenom:</html>"));
+	content_nb.add(new JLabel("<html>Mela: <br/>Nombre App: </html>"));
 	content_periph.add(view_periph);
 	content_modifier.add(modifier_profil);  
-	content_reco_app1.add(new JLabel("Appli 1"));
-	content_reco_app2.add(new JLabel("Appli 2")  ); 
-	content_reco_app3.add(new JLabel("Appli 3")   );
-	content_top_app1.add(new JLabel("Appli 1")  );
-	content_top_app2.add(new JLabel("Appli 2")  );
-	content_top_app3.add(new JLabel("Appli 3"))   ;
+	content_reco_app1.add(new JButton("<html>Appli 1<br/>OS: type:<html>"));
+	content_reco_app2.add(new JButton("<html>Appli 2<br/>OS: type:<html>")); 
+	content_reco_app3.add(new JButton("<html>Appli 3<br/>OS: type:<html>"));
+	content_top_app1.add(new JButton("<html>App1 etoiles<br/>nbcommentaires:</html>"));
+	content_top_app2.add(new JButton("<html>App2 etoiles<br/>nbcommentaires:</html>"));
+	content_top_app3.add(new JButton("<html>App3 etoiles<br/>nbcommentaires:</html>"));
 	content_top_view.add(view_more_top)   ;
 	content_reco_view.add(view_more_reco);
-	
+	// OUEST
 	conteneur_ouest.add(content_deco);
 	conteneur_ouest.add(content_titre_bienvenue);
 	conteneur_ouest.add(content_info);
 	conteneur_ouest.add(content_nb);
 	conteneur_ouest.add(content_periph);
 	conteneur_ouest.add(content_modifier);
-
+	// CENTRE
 	conteneur_centre.add(content_recherche);
 	conteneur_centre.add(content_titre_reco);
 	conteneur_centre.add(content_reco_app1);
 	conteneur_centre.add(content_reco_app2);
 	conteneur_centre.add(content_reco_app3);
 	conteneur_centre.add(content_reco_view);
-
+	// EST
 	conteneur_est.add(content_tout);
 	conteneur_est.add(content_titre_top);
 	conteneur_est.add(content_top_app1);
 	conteneur_est.add(content_top_app2);
 	conteneur_est.add(content_top_app3);
 	conteneur_est.add(content_top_view);
-
+	// ADD
 	add("East", conteneur_est);
 	add("Center", conteneur_centre);	
 	add("West", conteneur_ouest);
