5567398a493d9c596f6a9c474b9044a9ed572ac2
Thomas Salmon <th_s@hotmail.fr>
2013-05-04 21:25:49 +0200

    LookAndFeel

--- projet/src/client/Client.java
+++ projet/src/client/Client.java
@@ -1,6 +1,9 @@
 //package client;
 import java.util.Scanner;
 import java.awt.Color;
+import java.util.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 public class Client 
 {
     /* var affichage */
@@ -96,9 +99,25 @@ public class Client
     public static void main(String[] args)
     {
 	//connect();
-	
-	Client.getInstance().setFen(new Fenetre());
-	Client.getInstance().getFen().setContentPane(new MenuConnexion("GoldenStore - Connexion"));
-	
+	javax.swing.SwingUtilities.invokeLater(new Runnable()
+	    {
+		public void run()
+		{
+		    try{
+			for(UIManager.LookAndFeelInfo info: UIManager.getInstalledLookAndFeels())
+			    {
+				if("Nimbus".equals(info.getName())){
+				    UIManager.setLookAndFeel(info.getClassName());
+				    break;
+				}
+			    }
+		    }
+		    catch(Exception e){
+			e.printStackTrace();
+		    }
+		    Client.getInstance().setFen(new Fenetre());
+		    Client.getInstance().getFen().setContentPane(new MenuConnexion("GoldenStore - Connexion"));
+		}
+	    });
     }
 }
