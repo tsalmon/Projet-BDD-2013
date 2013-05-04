2058121abb51f30cd1d8ab9c34a47362d534b241
Thomas Salmon <th_s@hotmail.fr>
2013-05-04 03:05:58 +0200

    preque fini, il reste le bouton ok a traiter

--- projet/src/client/ModifierCompte.java
+++ projet/src/client/ModifierCompte.java
@@ -17,7 +17,7 @@ public class ModifierCompte extends JPanel implements MouseListener
 {
     
     private SqlData info = Client.getInstance().getConnect().request("get_infoMe");
-    private SqlData paye = Client.getInstance().getConnect().request("get_infoMe");
+    private SqlData paye = Client.getInstance().getConnect().request("get_paiementMe");
 
     JPanel header = new JPanel();
     JScrollPane centre = new JScrollPane();
@@ -26,6 +26,11 @@ public class ModifierCompte extends JPanel implements MouseListener
     JButton ok = new JButton("Ok");
     JButton accueil = new JButton("Accueil");
     JButton deconnexion = new JButton("Déconnexion");
+
+    JTextField new_mail = new JTextField(10);
+    JTextField new_mdp = new JTextField(10);
+    JTextField conf_mail = new JTextField(10);
+    JTextField conf_mdp = new JTextField(10);
 
     public ModifierCompte()
     {
@@ -60,14 +65,79 @@ public class ModifierCompte extends JPanel implements MouseListener
 
     public void addcentre()
     {
-	//SqlData paye = Client.getInstance().getConnect().request("get_infoMe");
-	paye.read_sqldata();
 	JPanel grille = new JPanel();
-	grille.setLayout(new GridLayout(3, 1));
 	JPanel content_mail = new JPanel();
 	JPanel content_mdp = new JPanel();
 	JPanel content_payement = new JPanel();
+	JPanel content_new_mail = new JPanel();
+	JPanel content_conf_mail = new JPanel();
+	JPanel content_new_mdp = new JPanel();
+	JPanel content_conf_mdp = new JPanel();
+	JPanel content_champs_mail = new JPanel();
+	JPanel content_champs_mdp = new JPanel();
+	
+	grille.setLayout(new GridLayout(4, 1));
+
+	grille.add(new JLabel("Ne mettez rien dans les champs de mots de passe ou d'email si vous ne souhaitez pas les changer"));
+	
+	// adresse mail
+	content_mail.setLayout(new BorderLayout());
+
+	content_new_mail.setLayout(new BorderLayout());
+	content_new_mail.add("North", new JLabel("Nouvelle adresse:"));
+	content_new_mail.add("Center", new_mail);
+
+	content_conf_mail.setLayout(new BorderLayout());
+	content_conf_mail.add("North", new JLabel("Confirmer:"));
+	content_conf_mail.add("Center", conf_mail);
+	
+	content_champs_mail.setLayout(new GridLayout(1, 2));
+	content_champs_mail.add(content_new_mail);
+	content_champs_mail.add(content_conf_mail);	
+	
+	content_mail.add("North", new JLabel("Changer d'adresse e-mail"));
+	content_mail.add("Center", content_champs_mail);
+	
+	grille.add(content_mail);	
+
+	// mot de passe 
+	content_mdp.setLayout(new BorderLayout());
+
+	content_new_mdp.setLayout(new BorderLayout());
+	content_new_mdp.add("North", new JLabel("Nouveau mot de passe:"));
+	content_new_mdp.add("Center", new_mdp);
+
+	content_conf_mdp.setLayout(new BorderLayout());
+	content_conf_mdp.add("North", new JLabel("Confirmer:"));
+	content_conf_mdp.add("Center", conf_mdp);
+	
+	content_champs_mdp.setLayout(new GridLayout(1, 2));
+	content_champs_mdp.add(content_new_mdp);
+	content_champs_mdp.add(content_conf_mdp);	
+	
+	content_mdp.add("North", new JLabel("Changer de mot de passe"));
+	content_mdp.add("Center", content_champs_mdp);
+
+	grille.add(content_mdp);
+
+	//paiement
+	content_payement.setLayout(new GridLayout(paye.getNbLigne(),1));
+	JPanel[] payement_grille = new JPanel[paye.getNbLigne()];
+	for(int i = 0 ; i < paye.getNbLigne(); i++)
+	    {
+		payement_grille[i] = new JPanel();
+		payement_grille[i].setLayout(new BorderLayout());
+		payement_grille[i].add("North", new JLabel((paye.data[i][1].equals("1")? "Carte Bleue" : "Paypal")));
+		payement_grille[i].add("Center", new JLabel(paye.data[i][2]));
+		if(paye.data[i][1].equals("1"))
+		    payement_grille[i].add("South", new JLabel(paye.data[i][2]));
+		content_payement.add(payement_grille[i]);
+	    }
+	
+	grille.add(content_payement);
+	
 	centre = new JScrollPane(grille);
+	
     }
     
     public void addsud()
@@ -92,7 +162,14 @@ public class ModifierCompte extends JPanel implements MouseListener
 		Client.getInstance().getConnect().dialog("DISCONNECT");
                 Client.getInstance().getFen().setContentPane(new MenuConnexion("GoldenStore - Connexion"));
 	    }
-	
+	if(e.getSource() == ok)
+	    {
+		
+	    }
+	if(e.getSource() == annuler)
+	    {
+		Client.getInstance().getFen().setContentPane(new Accueil());
+	    }
     }
     public void mouseEntered(MouseEvent e){}
     public void mouseExited(MouseEvent e){}
