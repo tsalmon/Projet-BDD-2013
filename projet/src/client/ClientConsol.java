f666a5e8b5f86871a672ed8497fe4e753bc67c98
beziau paul <bibibobe@ubuntu.(none)>
2013-05-05 18:54:36 +0200

    client en mode consol avec petit graphique

---
+++ projet/src/client/ClientConsol.java
@@ -0,0 +1,96 @@
+
+import java.util.Scanner;
+
+
+
+public class ClientConsol {
+
+  public static final int PORT = 25567;
+  public static final String HOST = "multi-craft.fr";
+  public static boolean auth=false;
+
+  public static void main(String []arguments) {
+    ConnexionClient c = new ConnexionClient(); //se connecte au serveur
+    SqlChart chart = new SqlChart();
+	//if(c.auth("Stuart","408101")){System.out.println("auth reussi");}
+//	else{System.out.println("connexion fail");}	// s'authentifie
+	Scanner scanner= new Scanner(System.in);
+	System.out.println("taper \"CONNEXION (mail)\" pour vous identifier, \"HELP\" pour plus d'informations");
+	while(true){
+		String chaine=scanner.nextLine();
+		if(chaine.charAt(0)=='R' && chaine.charAt(2)=='Q'){
+			String[] chaineTrie =chaine.split("#");
+			chaine=chaineTrie[0].replace("REQUEST ", "");
+			SqlData s =c.request(chaine); // execute la requete list_client (=> "SELECT * FROM client") et recupere les donnes dans SqlData 
+			
+			if(s!=null) { //on verifie que SqlData n'est pas null (e.q que le serveur a bien renvoyer des donnees)
+				
+				if(chaineTrie.length>1){s.trie(Integer.parseInt(chaineTrie[1]));}
+				
+				String[] chaineSplit=chaine.split("[|]");
+				chart.print(chaineSplit[0], s);
+				
+				int[] tab = new int[s.getNbCol()];
+			//	s.trie(4);
+				
+				for(int i=0;i<s.getNbCol();i++)
+					tab[i]=(s.getNomCol(i).length()>tab[i])?s.getNomCol(i).length():tab[i];
+			
+				for(int l=0; l<s.getNbLigne();l++)
+					for(int i=0;i<s.getNbCol();i++)
+						tab[i]=(s.getString(l, i).length()>tab[i])?s.getString(l, i).length():tab[i];
+						
+						
+				for(int i=0;i<s.getNbCol();i++){
+					String col= s.getNomCol(i);
+					String vide="";
+					for(int k=0;k<tab[i]-col.length();k++)vide+=" ";
+					System.out.print("|"+col+""+vide);
+					
+				}
+				System.out.println("");
+				for(int l=0; l<s.getNbLigne();l++){
+					for(int i=0;i<s.getNbCol();i++){
+						String col= s.getNomCol(i);
+						String val= s.getString(l, i);
+						String vide="";
+						for(int k=0;k<tab[i]-val.length();k++)vide+=" ";
+						System.out.print("|"+val+""+vide);
+						
+					}
+					System.out.println("");
+				}
+				System.out.println("");
+			}
+		}
+		else if(chaine.charAt(0)=='C' && chaine.charAt(1)=='O'){
+			if(!auth){
+				String user = chaine.replace("CONNEXION ", "");
+				System.out.print("veuillez entrer votre mot de pass :");
+				String mdp =scanner.nextLine();
+				auth=c.auth(user,mdp);
+				System.out.println((auth)?"connexion reussi":"echec de connexion");
+			}
+			else {
+				System.out.println("vous etes deja connecter");
+			}
+		}
+		else if(chaine.charAt(0)=='R'){
+			chaine=chaine.replace(" ", "|");
+			System.out.println(c.dialog(chaine));
+		}
+		else if(chaine.equals("DISCONNECT")){
+			auth=false;
+			System.out.println(c.dialog(chaine));
+		}
+		else if(chaine.equals("CLOSE")){
+			c.exit();
+			System.exit(1);
+		}
+		else {
+			System.out.println(c.dialog(chaine));
+		}
+		
+	}
+  }
+}
---
+++ projet/src/client/SqlChart.java
@@ -0,0 +1,105 @@
+import  org.jfree.chart.ChartFactory;
+import  org.jfree.chart.ChartFrame;
+import  org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+import  org.jfree.data.general.DefaultPieDataset;
+import org.jfree.util.Rotation;
+
+
+public class SqlChart {
+
+	private String[][] dessinable;
+	
+	
+	  public SqlChart(){
+		  this.dessinable = new String[2][2];
+		  this.dessinable[0][0]="get_developpeurTop";this.dessinable[0][1]="barChart";
+		  this.dessinable[1][0]="get_nbAppPeriphType";this.dessinable[1][1]="pieChart";
+	  }
+	  
+	  @SuppressWarnings("deprecation")
+	public void print(String nom,SqlData s) {
+		//create a dataset...*
+		int estDessinable=-1;  
+		  
+		for(int i=0;i<this.dessinable.length;i++){
+			if(this.dessinable[i][0].equals(nom)){estDessinable=i;break;}
+		}
+		
+		
+		if(estDessinable==-1)return ;
+		
+		if(this.dessinable[estDessinable][1].equals("pieChart")){
+			DefaultPieDataset dataset=new DefaultPieDataset();
+			
+			for(int i=0;i<s.getNbLigne();i++)
+			   dataset.setValue(s.getString(i, 0),s.getInt(i, 1));
+	
+			//create a chart...
+			JFreeChart chart=ChartFactory.createPieChart3D( 
+					   nom,
+					   dataset,
+					   true,
+					   true,
+					   false
+		       );
+			//create and display a frame...
+			
+			final PiePlot3D plot = (PiePlot3D) chart.getPlot();
+	        plot.setStartAngle(290);
+	        plot.setDirection(Rotation.CLOCKWISE);
+	        plot.setForegroundAlpha(0.5f);
+	        plot.setNoDataMessage("No data to display");
+	        
+	        ChartFrame frame=new ChartFrame("First",chart);
+			frame.pack();
+			frame.setVisible(true);
+		}
+		
+		
+		else if(this.dessinable[estDessinable][1].equals("barChart")){
+			
+			DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	        
+			for(int i=0;i<s.getNbLigne();i++)
+			    dataset.addValue(s.getInt(i,1), s.getString(i,0), s.getNomCol(1));   
+	        
+	        JFreeChart chart = ChartFactory.createBarChart3D(
+	                nom,      // chart title
+	                s.getNomCol(0),               // domain axis label
+	                s.getNomCol(1),                  // range axis label
+	                dataset,                  // data
+	                PlotOrientation.VERTICAL, // orientation
+	                true,                     // include legend
+	                true,                     // tooltips
+	                false                     // urls
+	            );
+	        
+	        final CategoryPlot plot = chart.getCategoryPlot();
+	        final CategoryAxis axis = plot.getDomainAxis();
+	        axis.setCategoryLabelPositions(
+	            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 8.0)
+	        );
+	        
+	        final CategoryItemRenderer renderer = plot.getRenderer();
+	        renderer.setItemLabelsVisible(true);
+	        final BarRenderer r = (BarRenderer) renderer;
+	        r.setMaximumBarWidth(0.05);
+	        
+			ChartFrame frame=new ChartFrame("First",chart);
+			frame.pack();
+			frame.setVisible(true);
+		}
+        
+        
+	
+	  }
+}
+
